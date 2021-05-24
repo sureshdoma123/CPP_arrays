d39811a5359a48fc4b5a182ac266cfefdc1aa047
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-24 14:34:13 +0530

    Add files via upload

--- Max_custom_visit_count.cpp
+++ Max_custom_visit_count.cpp
@@ -1,4 +1,5 @@
-//To calulate the customer max visit count
+/*
+//using normal method
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -30,3 +31,64 @@ int main()
 	}
 	cout<<"The customer "<<m<<" is reapted for "<<mc<<" times";	
 }
+*/
+//using map method
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	int n,arr[n];
+	cout<<"Enter the size of the arry: ";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	//int *min=min_element(arr,arr+n);
+	//int *max=max_element(arr,arr+n);
+	map<int,int>dic;
+	for(int i=0;i<n;i++)
+	{
+		dic[arr[i]]++;
+	}
+	int mc=0,id=0;
+	for(auto it:dic)
+	{
+		if(mc<it.second)
+		{
+			mc=it.second;
+			id=it.first;
+		}
+	}
+	cout<<"custotm id is: "<<id<<endl;
+	cout<<"max count is: "<<mc;
+
+	
+}
+/*
+//Using Frequency Method
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	int n,arr[n];
+	cout<<"Enter size of the array: ";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	//int *min=min_element(arr,arr+n);
+	int *max=max_element(arr,arr+n);
+	cout<<"Max element is: "<<*max<<endl;
+	int res[*max+1]={0};
+	for(int i=0;i<n;i++)
+	{
+		res[arr[i]]++;
+	}
+	for(int i=0;i<*max;i++)
+	{
+		cout<<res[i]<<" ";
+	}
+}
+*/
