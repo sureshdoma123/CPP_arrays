10a38c734d45e6721571c75815b14df031e792c6
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-27 13:06:09 +0530

    Add files via upload

---
+++ Search_element_in_vector.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int n,val;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>val;
+        v.push_back(val);
+    }
+    //Method 1(by value)
+    /*int x;
+    cout<<"Enter the element to find in vector: ";
+    cin>>x;
+    vector<int>::iterator it;
+    it=find(v.begin(),v.end(),x);
+    if(*it==x)
+    {
+        cout<<x<<" is Found";
+    }
+    else
+    {
+        cout<<x<<" is not found ";
+    }*/
+    
+    //Method 2(by index)
+    int x;
+    cout<<"Enter x value : ";
+    cin>>x;
+    vector<int>::iterator it;
+    it=find(v.begin(),v.end(),x);
+    if(it!=v.end())
+    {
+        cout<<it-v.begin();
+    }
+    else
+    {
+        cout<<-1;
+    }
+
+
+
+}
